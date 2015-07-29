efa83e9f26c5818f4339a901e11d7dc654f3d1ab
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-07-30 02:29:14 +0300

    Fix indentation

--- pe005/pe005.hs
+++ pe005/pe005.hs
@@ -1,15 +1,15 @@
 import PE.Primes
 
 main = do
-	print . product . foldl primeOrNotCovered ps $ [1..n]
-	where
-		primeOrNotCovered xs x
-			| isPrime x = xs
-			| otherwise = foldl notCovered x xs:xs
-		notCovered 1 _ = 1
-		notCovered x p
-			| x `mod` p == 0 = x `div` p
-			| otherwise = x
-		isPrime x = x `elem` ps
-		ps = takeWhile (<=n) primes
-		n = 20
+    print . product . foldl primeOrNotCovered ps $ [1..n]
+    where
+        primeOrNotCovered xs x
+            | isPrime x = xs
+            | otherwise = foldl notCovered x xs:xs
+        notCovered 1 _ = 1
+        notCovered x p
+            | x `mod` p == 0 = x `div` p
+            | otherwise = x
+        isPrime x = x `elem` ps
+        ps = takeWhile (<=n) primes
+        n = 20
