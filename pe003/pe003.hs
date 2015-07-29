70299f16d2eeca69690dd04852a89efa36fa6d4a
Valentine Paramonov <valentins.paramonovs@4finance.com>
2015-07-29 17:40:20 +0300

    Fix indentation

--- pe003/pe003.hs
+++ pe003/pe003.hs
@@ -1,4 +1,4 @@
 import PE.Primes
 
 main = do
-	print . foldl1 max . primeFactors $ 600851475143
+    print . foldl1 max . primeFactors $ 600851475143
--- pe004/pe004.hs
+++ pe004/pe004.hs
@@ -1,11 +1,11 @@
 main = do
-	print . foldl1 max . filter isPalindrome $ threeDigitNumberProducts
-	where
-		isPalindrome = isP . show
-		isP [] = True
-		isP [x] = True
-		isP (x:xs)
-			| x == (last xs) = isP $ init xs
-			| otherwise      = False
-		threeDigitNumberProducts = [ i * j | i <- tripleDigitNumbers, j <- tripleDigitNumbers]
-		tripleDigitNumbers = [100..999]
+    print . foldl1 max . filter isPalindrome $ threeDigitNumberProducts
+    where
+        isPalindrome = isP . show
+        isP [] = True
+        isP [x] = True
+        isP (x:xs)
+            | x == (last xs) = isP $ init xs
+            | otherwise      = False
+        threeDigitNumberProducts = [ i * j | i <- tripleDigitNumbers, j <- tripleDigitNumbers]
+        tripleDigitNumbers = [100..999]
