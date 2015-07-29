17280be020ec9046bafe7de639e45642d2770780
Valentine Paramonov <valentins.paramonovs@4finance.com>
2015-07-29 10:21:42 +0300

    Fix indentation

--- pe002/pe002.hs
+++ pe002/pe002.hs
@@ -1,5 +1,5 @@
 import PE.Math
 
 main = do
-		let limit = 4*10^6
-		print . sum . takeWhile (<=limit) . filter (even) $ fibs
+    let limit = 4*10^6
+    print . sum . takeWhile (<=limit) . filter (even) $ fibs
