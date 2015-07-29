76d559a2d5ba0799d0a9eb54e7ee1b3d0c79d2de
Valentine Paramonov <valentins.paramonovs@4finance.com>
2015-07-29 10:17:21 +0300

    Improve solution for problem #2

--- pe002/pe002.hs
+++ pe002/pe002.hs
@@ -1,11 +1,5 @@
+import PE.Math
 
-					-- Problem 2 --
-
-fib 0  = []
-fib 1  = [1]
-fib 2  = [2,1]
-fib n = (x1 + x2) : prev
-	where prev = fib $ n - 1
-	      (x1:x2:_) = prev
-
---sum([x | x <- (fib 100), x <= 4e6, even x])
+main = do
+		let limit = 4*10^6
+		print . sum . takeWhile (<=limit) . filter (even) $ fibs
