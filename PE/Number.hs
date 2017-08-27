237bbc56be2b6344c155f8909e47240d740de531
valentine <valentin.paramonov@protonmail.com>
2017-08-27 23:48:52 +0300

    Add function to count divisors

--- PE/Number.hs
+++ PE/Number.hs
@@ -1,6 +1,9 @@
-module PE.Number where
+module PE.Number (
+    toNumber, toDigits, numDivisors
+) where
 
 import Data.Char
+import PE.Primes
 
 toNumber :: (Integral a, Read a) => [Int] -> a
 toNumber = read . map intToDigit
@@ -12,3 +15,13 @@ rotate times n =
     let digits = toDigits n
     in toNumber $ take (length digits) $ drop times $ cycle digits
 
+countDivisors [] = 1
+countDivisors divisors@(d:ds) =
+    let 
+        isPower n = mod n d == 0
+        xPowers = takeWhile isPower divisors
+        rest = dropWhile isPower divisors
+    in
+        (length xPowers + 1) * (countDivisors rest)
+    
+numDivisors = countDivisors . primeFactors
