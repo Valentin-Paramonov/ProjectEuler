add1bc2c16a23254230b4fbc53ba805e16ae7c62
valentine <valentin.paramonov@protonmail.com>
2017-08-27 10:49:05 +0300

    Add function to rotate digits in a number

--- PE/Number.hs
+++ PE/Number.hs
@@ -7,3 +7,8 @@ toNumber = read . map intToDigit
 
 toDigits :: (Integral a, Show a) => a -> [Int]
 toDigits = map digitToInt . show
+
+rotate times n =
+    let digits = toDigits n
+    in toNumber $ take (length digits) $ drop times $ cycle digits
+
