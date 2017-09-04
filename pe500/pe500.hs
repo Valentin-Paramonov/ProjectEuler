cb2970dbe2bde4a000ba973cad4e56c3561abc8d
valentine <valentin.paramonov@protonmail.com>
2017-09-04 14:04:26 +0300

    Another unoptimal solution for problem #500

--- pe500/pe500.hs
+++ pe500/pe500.hs
@@ -1,25 +1,23 @@
 import PE.Primes
 
-prime n = primes !! n
+ps = take 500500 primes
+prime n = ps !! n
 
-update [] d = [d]
-update (d:ds) n
-    | d == n = d * d * d:ds
-    | otherwise = d:(update ds n)
-
-f divisors n
-    | n == 1 = product divisors
-    | otherwise = 
-        let 
-            p = prime $ length divisors
-            updatePossibility = dropWhile (\d -> d*d > p) divisors
-        in
-            if updatePossibility /= []
-            then
-                f (update divisors $ head updatePossibility) (n - 1)
-            else
-                f (update divisors p) (n - 1)
+f i 0 = 1
+f i d =
+    minimum $ map (\(c,n) -> pi^(c-1) * (f (i + 1) (d - n))) candidates
+    where
+        k = d
+        p = prime $ k + i - 1 - 1
+        pi = prime $ i - 1
+        m = floor (log (fromIntegral p) / log (fromIntegral pi))
+        powersOf2 = [(2^a,a+1) | a <- [1..]]
+        candidates = 
+            let 
+                cs = takeWhile ((<= m) . fst) powersOf2
+            in
+                if cs /= [] then map (\(c,n) -> (2*c,n)) cs else [(2,1)] 
 
 main = do
-    print $ mod (f [2] 500500) 500500507
+    print $ f 1 500500 `mod` 500500507
 
